ad04f9535c0b0e236d0159fce13b32caf583bd90
Dhananjayji <Dhananjayji@users.noreply.github.com>
2026-05-12 17:57:59 +0530

    connect firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_FIREBASE_API_KEY",
-  authDomain: "PASTE_FIREBASE_AUTH_DOMAIN",
-  projectId: "PASTE_FIREBASE_PROJECT_ID",
-  storageBucket: "PASTE_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "PASTE_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "PASTE_FIREBASE_APP_ID",
+  apiKey: "AIzaSyAfjexoFckg5QP_01zJKWhV0KvdkzsGG28",
+  authDomain: "djayblogs.firebaseapp.com",
+  projectId: "djayblogs",
+  storageBucket: "djayblogs.firebasestorage.app",
+  messagingSenderId: "663110751890",
+  appId: "1:663110751890:web:c9015e9f4e6e2800cff84b",
 };
 
 export const adminEmail = "djay@djayblogs.local";
